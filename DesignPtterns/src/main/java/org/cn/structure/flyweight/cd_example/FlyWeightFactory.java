47b7685455fc209c3fa2b231e020af68f88dff6a
李洪智 <123456789>
2018-11-01 16:48:28 +0800

    ConcurrentHashMap

--- DesignPtterns/src/main/java/org/cn/structure/flyweight/cd_example/FlyWeightFactory.java
+++ DesignPtterns/src/main/java/org/cn/structure/flyweight/cd_example/FlyWeightFactory.java
@@ -1,12 +1,12 @@
 package org.cn.structure.flyweight.cd_example;
 
-import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *  享元工厂类
  */
 public class FlyWeightFactory {
-    private static Hashtable pool = new Hashtable();
+    private static ConcurrentHashMap pool = new ConcurrentHashMap();
 
     public static Artist getArtist(String name){
         Artist artist = (Artist) pool.get(name);
