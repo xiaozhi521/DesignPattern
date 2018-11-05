1ebeab6a6b26a8c663942eb16f3956a62da1e667
李洪智 <123456789>
2018-11-05 14:34:51 +0800

    修订

---
+++ DesignPtterns/src/main/java/org/cn/behavior/observer/product_spring/SmsListener.java
@@ -0,0 +1,32 @@
+package org.cn.behavior.observer.product_spring;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.SmartApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SmsListener implements SmartApplicationListener {
+    //监听哪一种事件
+    @Override
+    public boolean supportsEventType(Class<? extends ApplicationEvent> aClass) {
+        return aClass == OrderEvent.class;
+    }
+
+    @Override
+    public boolean supportsSourceType(Class<?> aClass) {
+        return true;
+    }
+
+    //当事件发生时执行
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+
+    }
+
+    //定义多个监听者的执行顺序
+    @Override
+    public int getOrder() {
+        //值越大，顺序越往后
+        return 60;
+    }
+}
